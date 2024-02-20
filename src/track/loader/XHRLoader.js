b1eb02ce849ace14c996180894220a3c2294964b
yarikLev <123753170+yarikLev@users.noreply.github.com>
2024-02-20 23:21:42 +0100

    fix logs

--- src/track/loader/XHRLoader.js
+++ src/track/loader/XHRLoader.js
@@ -15,13 +15,11 @@ export default class extends Loader {
       xhr.addEventListener("progress", (e) => {
         super.fileProgress(e);
 
-        const progressInPercents = (e.loaded / e.total) * 100
+        console.log('loaded', e.loaded)
+        console.log('total', e.total)
+        console.log('total', (e.loaded / e.total) * 100)
 
-        console.log(progressInPercents)
-
-        console.log(handleProgress(progressInPercents))
-
-        handleProgress(progressInPercents);
+        handleProgress((e.loaded / e.total) * 100);
       });
 
       xhr.addEventListener("load", (e) => {
