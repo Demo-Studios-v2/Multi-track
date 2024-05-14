a796e4857b5dd8e1eda0bf93ed297bc1063843b2
16Bogdan54 <b.yunakov1654@gmail.com>
2024-05-14 19:54:58 +0300

    increase width

--- src/Track.js
+++ src/Track.js
@@ -180,6 +180,12 @@ export default class {
     const cueIn = secondsToSamples(this.cueIn, sampleRate);
     const cueOut = secondsToSamples(this.cueOut, sampleRate);
 
+    console.log("Peaks before extraction",         this.buffer,
+      samplesPerPixel,
+      this.peakData.mono,
+      cueIn,
+      cueOut);
+
     this.setPeaks(
       extractPeaks(
         this.buffer,
